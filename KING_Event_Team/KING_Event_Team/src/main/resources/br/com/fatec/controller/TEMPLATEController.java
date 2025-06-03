ad2e6f815be697ec54bbec66b27643568fff79b3
LuanBMDev <luannbm@gmail.com>
2025-06-02 22:59:20 -0300

    Criando um controler TEMPLATE

---
+++ KING_Event_Team/KING_Event_Team/src/main/resources/br/com/fatec/controller/TEMPLATEController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package br.com.fatec.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author luann
+ */
+public class TEMPLATEController implements Initializable {
+
+    @FXML
+    private Button btnFechar;
+    @FXML
+    private Button btnMin;
+    @FXML
+    private Button btnVoltar;
+    @FXML
+    private Label lblTitulo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnFechar_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnMiin_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnVoltar_Click(ActionEvent event) {
+    }
+    
+}
