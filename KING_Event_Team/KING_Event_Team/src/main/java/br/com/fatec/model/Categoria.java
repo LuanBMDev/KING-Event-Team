a201b13db22ca08d1490580ac783a3a09801c098
LuanBMDev <luannbm@gmail.com>
2025-06-05 11:55:44 -0300

    Add model/Categoria e Localizacoes

---
+++ KING_Event_Team/KING_Event_Team/src/main/java/br/com/fatec/model/Categoria.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.fatec.model;
+
+/**
+ *
+ * @author luann
+ */
+public class Categoria 
+{
+    private int codCat;
+    private String nomeCat;
+
+    public Categoria() {
+    }
+
+    public Categoria(int codCat, String nomeCat) {
+        this.codCat = codCat;
+        this.nomeCat = nomeCat;
+    }
+
+    public int getCodCat() {
+        return codCat;
+    }
+
+    public void setCodCat(int codCat) {
+        this.codCat = codCat;
+    }
+
+    public String getNomeCat() {
+        return nomeCat;
+    }
+
+    public void setNomeCat(String nomeCat) {
+        this.nomeCat = nomeCat;
+    }
+}
---
+++ KING_Event_Team/KING_Event_Team/src/main/java/br/com/fatec/model/Localizacoes.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.fatec.model;
+
+/**
+ *
+ * @author luann
+ */
+public class Localizacoes 
+{
+    private int codLocal;
+    private String nomeLocal;
+    private String CEP;
+    private String enderecoLocal;
+    private int numeroLocal;
+    private String cidade;
+    private String tipoLocal;
+
+    public Localizacoes() {
+    }
+
+    public Localizacoes(int codLocal, String nomeLocal, String CEP, String enderecoLocal, int numeroLocal, String cidade, String tipoLocal) {
+        this.codLocal = codLocal;
+        this.nomeLocal = nomeLocal;
+        this.CEP = CEP;
+        this.enderecoLocal = enderecoLocal;
+        this.numeroLocal = numeroLocal;
+        this.cidade = cidade;
+        this.tipoLocal = tipoLocal;
+    }
+    
+    public int getCodLocal() {
+        return codLocal;
+    }
+
+    public void setCodLocal(int codLocal) {
+        this.codLocal = codLocal;
+    }
+
+    public String getNomeLocal() {
+        return nomeLocal;
+    }
+
+    public void setNomeLocal(String nomeLocal) {
+        this.nomeLocal = nomeLocal;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getEnderecoLocal() {
+        return enderecoLocal;
+    }
+
+    public void setEnderecoLocal(String enderecoLocal) {
+        this.enderecoLocal = enderecoLocal;
+    }
+
+    public int getNumeroLocal() {
+        return numeroLocal;
+    }
+
+    public void setNumeroLocal(int numeroLocal) {
+        this.numeroLocal = numeroLocal;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getTipoLocal() {
+        return tipoLocal;
+    }
+
+    public void setTipoLocal(String tipoLocal) {
+        this.tipoLocal = tipoLocal;
+    }
+    
+    
+}
