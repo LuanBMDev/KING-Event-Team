471c6a1990f96099da3d48b1f43969bfa6062121
LuanBMDev <luannbm@gmail.com>
2025-06-05 23:31:33 -0300

    Renomeando propriedades de model para encaixar no padrão de Banco de Dados

--- KING_Event_Team/KING_Event_Team/src/main/java/br/com/fatec/model/Categoria.java
+++ KING_Event_Team/KING_Event_Team/src/main/java/br/com/fatec/model/Categoria.java
@@ -13,27 +13,34 @@ public class Categoria
     private int codCat;
     private String nomeCat;
 
-    public Categoria() {
+    public Categoria() 
+    {
+        
     }
 
-    public Categoria(int codCat, String nomeCat) {
+    public Categoria(int codCat, String nomeCat) 
+    {
         this.codCat = codCat;
         this.nomeCat = nomeCat;
     }
 
-    public int getCodCat() {
+    public int getCodCat() 
+    {
         return codCat;
     }
 
-    public void setCodCat(int codCat) {
+    public void setCodCat(int codCat) 
+    {
         this.codCat = codCat;
     }
 
-    public String getNomeCat() {
+    public String getNomeCat() 
+    {
         return nomeCat;
     }
 
-    public void setNomeCat(String nomeCat) {
+    public void setNomeCat(String nomeCat) 
+    {
         this.nomeCat = nomeCat;
     }
 }
--- KING_Event_Team/KING_Event_Team/src/main/java/br/com/fatec/model/Localizacao.java
+++ KING_Event_Team/KING_Event_Team/src/main/java/br/com/fatec/model/Localizacao.java
@@ -13,79 +13,94 @@ public class Localizacao
     private int codLocal;
     private String nomeLocal;
     private String CEP;
-    private String enderecoLocal;
+    private String endereco;
     private int numeroLocal;
     private String cidade;
     private String tipoLocal;
 
-    public Localizacao() {
+    public Localizacao() 
+    {
+        
     }
 
-    public Localizacao(int codLocal, String nomeLocal, String CEP, String enderecoLocal, int numeroLocal, String cidade, String tipoLocal) {
+    public Localizacao(int codLocal, String nomeLocal, String CEP, String enderecoLocal, int numeroLocal, String cidade, String tipoLocal) 
+    {
         this.codLocal = codLocal;
         this.nomeLocal = nomeLocal;
         this.CEP = CEP;
-        this.enderecoLocal = enderecoLocal;
+        this.endereco = enderecoLocal;
         this.numeroLocal = numeroLocal;
         this.cidade = cidade;
         this.tipoLocal = tipoLocal;
     }
     
-    public int getCodLocal() {
+    public int getCodLocal() 
+    {
         return codLocal;
     }
 
-    public void setCodLocal(int codLocal) {
+    public void setCodLocal(int codLocal) 
+    {
         this.codLocal = codLocal;
     }
 
-    public String getNomeLocal() {
+    public String getNomeLocal() 
+    {
         return nomeLocal;
     }
 
-    public void setNomeLocal(String nomeLocal) {
+    public void setNomeLocal(String nomeLocal) 
+    {
         this.nomeLocal = nomeLocal;
     }
 
-    public String getCEP() {
+    public String getCEP() 
+    {
         return CEP;
     }
 
-    public void setCEP(String CEP) {
+    public void setCEP(String CEP) 
+    {
         this.CEP = CEP;
     }
 
-    public String getEnderecoLocal() {
-        return enderecoLocal;
+    public String getEndereco() 
+    {
+        return endereco;
     }
 
-    public void setEnderecoLocal(String enderecoLocal) {
-        this.enderecoLocal = enderecoLocal;
+    public void setEndereco(String endereco) 
+    {
+        this.endereco = endereco;
     }
 
-    public int getNumeroLocal() {
+    public int getNumeroLocal() 
+    {
         return numeroLocal;
     }
 
-    public void setNumeroLocal(int numeroLocal) {
+    public void setNumeroLocal(int numeroLocal) 
+    {
         this.numeroLocal = numeroLocal;
     }
 
-    public String getCidade() {
+    public String getCidade() 
+    {
         return cidade;
     }
 
-    public void setCidade(String cidade) {
+    public void setCidade(String cidade) 
+    {
         this.cidade = cidade;
     }
 
-    public String getTipoLocal() {
+    public String getTipoLocal() 
+    {
         return tipoLocal;
     }
 
-    public void setTipoLocal(String tipoLocal) {
+    public void setTipoLocal(String tipoLocal) 
+    {
         this.tipoLocal = tipoLocal;
     }
-    
-    
 }
